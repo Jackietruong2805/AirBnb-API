af89cc909b4f043d9a1bd15b3e79d2dc4bf18887
Jackietruong2805 <truonghao2805@gmail.com>
2022-11-28 13:06:18 +0700

    AirBnb.28.11.2022-02

--- AirBnb/src/Controllers/datPhongController.js
+++ AirBnb/src/Controllers/datPhongController.js
@@ -15,33 +15,48 @@ const getDatPhong = async (req, res) =>{
     }
 }
 
-const createDatPhong = async (req, res) =>{
+const getDatPhongById = async (req, res) =>{
     try{
-        let data = req.body;
-        const {id_nguoidung, id_phong, ngay_den, ngay_di} = data;
-        ngay_den_format = new Date(ngay_den);
-        ngay_di_format = new Date(ngay_di);
-        data = {...data, ngay_den: ngay_den_format, ngay_di: ngay_di_format};
-        const checkIdUser = await prisma.NguoiDung.findFirst({where: {id_nguoidung: +id_nguoidung}})
-        const checkIdPhong = await prisma.Phong.findFirst({where: {id_phong: +id_phong}})
-        if(checkIdUser){
-            if(checkIdPhong){
-                const result = await prisma.DatPhong.create({data});
-                if(result){
-                    createSuccessCode(res, result, "Thêm đặt phòng thành công");
-                }else{
-                    failCode(res, result, "Thêm phòng đặt thất bại");
-                }
-            }else{
-                failCode(res, checkIdPhong, "Phòng có id: " + id_phong + " không tồn tại");
-            }
+        const {id_nguoidung, id_phong} = req.params;
+        const result = await prisma.DatPhong.findFirst({where: {id_nguoidung: +id_nguoidung, id_phong: +id_phong}});
+        if(result){
+            successCode(res, result, "Lấy Phòng đặt thành công");
         }else{
-            failCode(res, checkIdUser, "Người Dùng có id: " + id_nguoidung + " không tồn tại");
+            failCode(res, result, "Lấy đặt phòng thất bại");
         }
     }catch(err){
         errorCode(res, err, "Lỗi backend");
     }
 }
 
+const createDatPhong = async (req, res) =>{
+    try{
+        let data = req.body;
+        const {id_nguoidung, id_phong, ngay_den, ngay_di} = data;
+        console.log(id_nguoidung, id_phong);
+        ngayDenFormat = new Date(ngay_den);
+        ngayDiFormat = new Date(ngay_di);
+        data = {...data, ngay_den: ngayDenFormat, ngay_di: ngayDiFormat};
+        const checkIdUser = await prisma.NguoiDung.findFirst({where: {id_nguoidung: +id_nguoidung}});
+        const checkIdPhong = await prisma.Phong.findFirst({where: {id_phong: +id_phong}});
+        const isIdAlReadyExist = await prisma.DatPhong.findFirst({where: {id_nguoidung: +id_nguoidung, id_phong: +id_phong}});
+        if(!isIdAlReadyExist){
+            if(checkIdUser){
+                if(checkIdPhong){
+                    const result = await prisma.DatPhong.create({data});
+                    createSuccessCode(res, result, "Thêm đặt phòng thành công");
+                }else{
+                    failCode(res, checkIdPhong, "Phòng có id: " + id_phong + " không tồn tại");
+                }
+            }else{
+                failCode(res, checkIdUser, "Người Dùng có id: " + id_nguoidung + " không tồn tại");
+            }
+        }else{
+            failCode(res, isIdAlReadyExist, "Đặt Phòng đã tồn tại");
+        }
+    }catch(err){
+        errorCode(res, err, "Lỗi backend");
+    }
+}
 
-module.exports = {getDatPhong, createDatPhong}
+module.exports = {getDatPhong, createDatPhong, getDatPhongById}
--- AirBnb/src/Routes/datPhongRoute.js
+++ AirBnb/src/Routes/datPhongRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const datPhongRoute = express.Router();
-const {getDatPhong, createDatPhong} = require('../Controllers/datPhongController');
+const {getDatPhong, createDatPhong, getDatPhongById} = require('../Controllers/datPhongController');
 
 datPhongRoute.get('/dat-phong', getDatPhong);
+datPhongRoute.get('/dat-phong/:id_nguoidung/:id_phong', getDatPhongById);
 datPhongRoute.post('/dat-phong', createDatPhong);
+
 
 module.exports = datPhongRoute;
