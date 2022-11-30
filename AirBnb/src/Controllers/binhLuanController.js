741978d39142c5eae311b5dc39c5aa8b5122400a
Jackietruong2805 <truonghao2805@gmail.com>
2022-11-30 16:35:21 +0700

    AirBnbApi.30.11.2022-02

--- AirBnb/src/Controllers/binhLuanController.js
+++ AirBnb/src/Controllers/binhLuanController.js
@@ -16,4 +16,27 @@ const getBinhLuan = async (req, res) =>{
     }
 }
 
-module.exports = {getBinhLuan}
+const createBinhLuan = async (req, res) =>{
+    try{
+        let data = req.body;
+        let {id_nguoidung, id_phong, ngay_binh_luan} = data;
+        ngay_binh_luan = new Date(ngay_binh_luan);
+        data = {...data, ngay_binh_luan};
+        const isUserExist = await prisma.NguoiDung.findFirst({where: {id_nguoidung: +id_nguoidung}});
+        const isPhongExist = await prisma.Phong.findFirst({where: {id_phong: +id_phong}});
+        if(isUserExist){
+            if(isPhongExist){
+                const result = await prisma.BinhLuan.create({data});
+                createSuccessCode(res, result, "Thêm bình luận thành công");
+            }else{
+                failCode(res, isPhongExist, "Phòng có id: " + id_phong + " không tồn tại");
+            }
+        }else{
+            failCode(res, isUserExist, "User có id: " + id_nguoidung + " không tồn tại");
+        }
+    }catch(err){
+        errorCode(res, err, "Lỗi backend");
+    }
+}
+
+module.exports = {getBinhLuan, createBinhLuan}
--- AirBnb/src/Routes/binhLuanRoute.js
+++ AirBnb/src/Routes/binhLuanRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const binhLuanRoute = express.Router();
 const {successCode, failCode, errorCode, createSuccessCode} = require('../ultis/response');
-const {getBinhLuan} = require('../Controllers/binhLuanController');
+const {getBinhLuan, createBinhLuan} = require('../Controllers/binhLuanController');
 
 binhLuanRoute.get('/binh-luan', getBinhLuan);
+binhLuanRoute.post('/binh-luan', createBinhLuan);
 
 module.exports = binhLuanRoute
