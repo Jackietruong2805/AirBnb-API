88d21e9e56ce00a18f2a0be3c3fa902d05a797b3
Jackietruong2805 <truonghao2805@gmail.com>
2022-12-04 12:04:19 +0700

    AirBnb.4.12.2022-01

--- AirBnb/src/Routes/authRoute.js
+++ AirBnb/src/Routes/authRoute.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const authRoute = express.Router();
+const {signUp, signIn} = require('../Controllers/authController');
 
+
+authRoute.post('/auth/signup', signUp);
+authRoute.post('/auth/signin', signIn);
 
 module.exports = authRoute;
