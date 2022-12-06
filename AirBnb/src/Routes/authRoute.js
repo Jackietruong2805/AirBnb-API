92c32adf44a303d8a56b5023dc9650f848bac5fd
Jackietruong2805 <truonghao2805@gmail.com>
2022-12-06 12:41:53 +0700

    AirBnb.6.12.2022-02

--- AirBnb/src/Routes/authRoute.js
+++ AirBnb/src/Routes/authRoute.js
@@ -2,7 +2,6 @@ const express = require('express');
 const authRoute = express.Router();
 const {signUp, signIn} = require('../Controllers/authController');
 
-
 authRoute.post('/auth/signup', signUp);
 authRoute.post('/auth/signin', signIn);
 
--- AirBnb/src/Routes/phongRoute.js
+++ AirBnb/src/Routes/phongRoute.js
@@ -2,15 +2,52 @@ const express = require('express');
 const phongRoute = express.Router();
 const {getPhong, createPhong, getPhongByVitri, getPhongById, updatePhong, deletePhong, uploadPhongImg} = require('../Controllers/phongController');
 const upload = require("../Middlewares/upload");
+const {failToken} = require('../ultis/response');
+const {checkToken} = require('../Middlewares/auth');
 
 phongRoute.get('/phong-thue', getPhong);
-phongRoute.post('/phong-thue', createPhong);
-phongRoute.post('/phong-thue/upload-hinh-phong', upload.single("imgPhong"), uploadPhongImg);
+phongRoute.post('/phong-thue', (req, res, next)=>{
+    try{
+        const {auth} = req.headers;
+        if(checkToken(auth)){
+            next();
+        }
+    }catch(err){
+        failToken(res, "Token không hợp lệ");
+    }
+} ,createPhong);
+phongRoute.post('/phong-thue/upload-hinh-phong', (req, res, next)=>{
+    try{
+        const {auth} = req.headers;
+        if(checkToken(auth)){
+            next();
+        }
+    }catch(err){
+        failToken(res, "Token không hợp lệ");
+    }
+}, upload.single("imgPhong"), uploadPhongImg);
 phongRoute.get('/phong-thue/lay-phong-theo-vi-tri', getPhongByVitri);
 phongRoute.get('/phong-thue/:id', getPhongById);
-phongRoute.put('/phong-thue/:id', updatePhong);
-phongRoute.delete('/phong-thue/:id', deletePhong);
+phongRoute.put('/phong-thue/:id', (req, res, next)=>{
+    try{
+        const {auth} = req.headers;
+        if(checkToken(auth)){
+            next();
+        }
+    }catch(err){
+        failToken(res, "Token không hợp lệ");
+    }
+} ,updatePhong);
 
-
+phongRoute.delete('/phong-thue/:id', (req, res, next)=>{
+    try{
+        const {auth} = req.headers;
+        if(checkToken(auth)){
+            next();
+        }
+    }catch(err){
+        failToken(res, "Token không hợp lệ");
+    }
+}, deletePhong);
 
 module.exports = phongRoute;
